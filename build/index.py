c676d377b34390521ce763c631dd1e9992a2cd72
Jochen Klar <mail@jochenklar.de>
2020-01-31 18:59:37 +0100

    Fix build/index.py

--- build/index.py
+++ build/index.py
@@ -18,6 +18,7 @@ def main():
     commit_date = subprocess.check_output(['git', 'show', '-s', '--format=%ci', 'HEAD']).decode().strip()
     commit_date = datetime.strptime(commit_date, '%Y-%m-%d %H:%M:%S %z').strftime('%d %B %Y')
 
+    # step 1: create hierarchy of simulation_rounds, products, and sectors
     pages = []
     for simulation_round in simulation_rounds:
         simulation_round['sectors'] = sectors
@@ -29,12 +30,11 @@ def main():
 
         pages.append(simulation_round)
 
+    # step 2: render content into layout template
     template_path = os.path.join('templates', 'index.html')
     output_path = os.path.join('output', 'index.html')
-
-    # step 4: render content into layout template
     enviroment = Environment(loader=FileSystemLoader(['templates']))
-
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
     with open(template_path) as f:
         template = enviroment.from_string(f.read())
     with open(output_path, 'w') as f:
