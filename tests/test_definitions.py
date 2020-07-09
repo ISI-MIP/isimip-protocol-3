ad310846df5963dc930070c5294663257a37bbf3
Jochen Klar <mail@jochenklar.de>
2020-07-09 14:58:52 +0200

    Add test_variable

--- tests/test_definitions.py
+++ tests/test_definitions.py
@@ -3,18 +3,13 @@ import os
 
 import jsonschema
 
-DOUBLES_WHITELIST = [
-    'landuse_variable.json'
-]
-
 
 def test_definitions():
     with open(os.path.join(os.path.dirname(__file__), 'meta.json')) as f:
         schema = json.loads(f.read())
 
-    path = 'definitions'
     for file_name in os.listdir('definitions'):
-        file_path = os.path.join(path, file_name)
+        file_path = os.path.join('definitions', file_name)
 
         with open(file_path) as f:
             instance = json.loads(f.read())
@@ -22,14 +17,34 @@ def test_definitions():
             # validate json with meta json
             jsonschema.validate(schema=schema, instance=instance)
 
-            # check for double specifiers
-            if file_name not in DOUBLES_WHITELIST:
-                seen = set()
-                doubles = []
-                for row in instance:
-                    if row['specifier'] in seen:
-                        doubles.append(row['specifier'])
-                    else:
-                        seen.add(row['specifier'])
 
-                assert not doubles, '{} {}'.format(file_name, doubles)
+def test_double_specifiers():
+    for file_name in os.listdir('definitions'):
+        file_path = os.path.join('definitions', file_name)
+
+        with open(file_path) as f:
+            instance = json.loads(f.read())
+
+            # check for double specifiers
+            seen = set()
+            doubles = []
+            for row in instance:
+                if row['specifier'] in seen:
+                    doubles.append(row['specifier'])
+                else:
+                    seen.add(row['specifier'])
+
+            assert not doubles, '{} {}'.format(file_name, doubles)
+
+
+def test_variable():
+    file_path = os.path.join('definitions', 'variable.json')
+    with open(file_path) as f:
+        instance = json.loads(f.read())
+        for row in instance:
+            sectors = row.get('sectors')
+            if sectors:
+                for key, value in row.items():
+                    if isinstance(value, dict):
+                        for sector in value:
+                            assert sector in sectors, row.get('specifier')
