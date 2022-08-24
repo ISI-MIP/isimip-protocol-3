3603d99d75f4a11d7d9d5ce1dd831cac3586d38c
Jochen Klar <mail@jochenklar.de>
2022-08-24 16:32:39 +0200

    Extend climate_specifiers table

--- app/src/components/tables/ClimateScenarioTable.js
+++ app/src/components/tables/ClimateScenarioTable.js
@@ -4,38 +4,58 @@ import PropTypes from 'prop-types'
 
 import SimulationRounds from '../badges/SimulationRounds'
 import Sectors from '../badges/Sectors'
+import Status from '../badges/Status'
 
-import { filterRows } from '../../utils'
+import { filterRows, filterField } from '../../utils'
 
 
 const ClimateScenarioTable = function({ config, number, rows }) {
   return (
-    <table className="table table-bordered table-fixed w-70">
+    <table className="table table-bordered table-fixed">
       <caption>
         Table {number}: Climate scenario specifiers (<code>climate-scenario</code>).
       </caption>
       <thead className="thead-dark">
         <tr>
           <th style={{width: '25%'}}>Scenario specifier</th>
-          <th style={{width: '50%'}}>Description</th>
+          <th style={{width: '25%'}}>Forcing</th>
+          <th style={{width: '15%'}}>Status</th>
+          <th style={{width: '35%'}}>Datasets</th>
         </tr>
       </thead>
       <tbody>
         {
           filterRows(config, rows).map((row, index) => {
-            return (
+            const forcings = filterField(config, row.forcings)
+            return forcings.map((forcing, index) => (
               <tr key={index}>
-                <td><strong>{row.specifier}</strong></td>
-                <td>
-                  <p>
-                    <SimulationRounds config={config} simulationRounds={row.simulation_rounds} />
-                    <Sectors config={config} sectors={row.sectors} />
-                  </p>
-                  <p>{row.description}</p>
-                  <ReactMarkdown children={row.description_note} />
+                {
+                  index == 0 && <td rowSpan={forcings.length} className="extra-border-bottom">
+                    <p>
+                      <strong>{row.specifier}</strong>
+                    </p>
+                    <p>
+                      <SimulationRounds config={config} simulationRounds={row.simulation_rounds} />
+                      <Sectors config={config} sectors={row.sectors} />
+                    </p>
+                  </td>
+                }
+                <td className={(index == forcings.length - 1) ? 'extra-border-bottom' : ''}>
+                  {forcing.forcing}
+                </td>
+                <td className={(index == forcings.length - 1) ? 'extra-border-bottom' : ''}>
+                  <Status status={forcing.status} />
+                </td>
+                <td className={(index == forcings.length - 1) ? 'extra-border-bottom' : ''}>
+                  {
+                    forcing.doi && <p>
+                      DOI: <a href={forcing.doi}>{ forcing.doi }</a>
+                    </p>
+                  }
+                  <ReactMarkdown children={forcing.comment} />
                 </td>
               </tr>
-            )
+            ))
           })
         }
       </tbody>
