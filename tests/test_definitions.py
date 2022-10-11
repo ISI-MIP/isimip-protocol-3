b50e55a0af851cd623d7b8859369370fbb3d1ca9
Jochen Klar <mail@jochenklar.de>
2022-10-11 16:50:54 +0200

    Fix tests

--- tests/test_definitions.py
+++ tests/test_definitions.py
@@ -41,6 +41,9 @@ def test_double_specifiers():
 
 
 def test_variable():
+    simulation_rounds = json.loads(Path('definitions').joinpath('simulation_round.json').read_text())
+    simulation_round_specifiers = [simulation_round['specifier'] for simulation_round in simulation_rounds]
+
     file_path = os.path.join('definitions', 'variable.json')
     with open(file_path) as f:
         instance = json.loads(f.read())
@@ -50,9 +53,12 @@ def test_variable():
                 for key, value in row.items():
                     if isinstance(value, dict):
                         field = '{}.{}'.format(row.get('specifier'), key)
-
-                        for sector in value:
-                            assert sector in sectors, field
+                        for key in value:
+                            if key in simulation_round_specifiers:
+                                for k in value[key]:
+                                    assert k in sectors, field
+                            else:
+                                assert key in sectors, field
 
 
 def test_dataset_groups():
@@ -78,3 +84,23 @@ def test_variable_groups():
         group = variable.get('group')
         assert group, variable.get('specifier')
         assert group in group_specifiers
+
+
+def test_nested_simulation_rounds():
+    simulation_rounds = json.loads(Path('definitions').joinpath('simulation_round.json').read_text())
+    simulation_round_specifiers = [simulation_round['specifier'] for simulation_round in simulation_rounds]
+
+    for file_name in os.listdir('definitions'):
+        if file_name not in ['experiments.json']:
+            file_path = os.path.join('definitions', file_name)
+
+            with open(file_path) as f:
+                instance = json.loads(f.read())
+
+                for row in instance:
+                    for value in row.values():
+                        if isinstance(value, dict):
+                            if simulation_round_specifiers[0] in value.keys():
+                                # assert that all (both simulation_round_specifiers) are there
+                                for simulation_round_specifier in simulation_round_specifiers[1:]:
+                                    assert simulation_round_specifier in value.keys(), row.get('specifier')
