5f367e2cd2e7f3d7a76d4d8b8b7ad57014941d4c
Jochen Klar <mail@jochenklar.de>
2022-11-09 16:32:55 +0100

    Add spin up note for marine-fishery and refactor Show component

--- app/src/components/Show.js
+++ app/src/components/Show.js
@@ -6,41 +6,35 @@ import SimulationRounds from './badges/SimulationRounds'
 import Sectors from './badges/Sectors'
 
 const Show = ({ config, simulationRound, sector, html }) => {
-  if (simulationRound !== undefined) {
-    const simulationRounds = simulationRound.split(',')
+  let className = 'show-component'
 
-    let className = 'show-component simulation_round'
+  const simulationRounds = simulationRound === undefined ? [] : simulationRound.split(',')
+  const sectors = sector === undefined ? [] : sector.split(',')
+
+  if (simulationRounds.length > 0) {
+    className += ' simulation_round'
+
     if (!simulationRounds.includes(config.simulation_round)) {
       className += ' hidden'
     }
+  }
+  if (sectors.length > 0) {
+    className += ' sectors'
 
-    return (
-      <div className={className}>
-        <div className="float-right">
-          <SimulationRounds config={config} simulationRounds={[simulationRound]} />
-        </div>
-        <div dangerouslySetInnerHTML={{__html: html}}></div>
-      </div>
-    )
-  } else if (sector !== undefined) {
-    const sectors = sector.split(',')
-
-    let className = 'show-component sectors'
     if (!(config.sectors.length == 0 || sectors.filter(sector => config.sectors.includes(sector)).length)) {
       className += ' hidden'
     }
-
-    return (
-      <div className={className}>
-        <div className="float-right">
-          <Sectors config={config} sectors={sectors} />
-        </div>
-        <div dangerouslySetInnerHTML={{__html: html}}></div>
-      </div>
-    )
   }
 
-  return null
+  return (
+    <div className={className}>
+      <div className="float-right">
+        {simulationRounds.length > 0 && <SimulationRounds config={config} simulationRounds={simulationRounds} />}
+        {sectors.length > 0 && <Sectors config={config} sectors={sectors} />}
+      </div>
+      <div dangerouslySetInnerHTML={{__html: html}}></div>
+    </div>
+  )
 }
 
 Show.propTypes = {
