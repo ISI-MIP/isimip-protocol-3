a757661aa0f5991ccf894a5a2dbbd2098a818a0d
Jochen Klar <mail@jochenklar.de>
2024-06-26 18:31:44 +0200

    Refactor group3 ranking table

--- app/src/components/tables/Group3RankingTable.js
+++ app/src/components/tables/Group3RankingTable.js
@@ -2,47 +2,36 @@ import React, { Component} from 'react'
 import ReactMarkdown from 'react-markdown'
 import PropTypes from 'prop-types'
 
-import { filterRows, filterField } from '../../utils'
+import { filterGroups, filterRows, filterField, GroupToggleLink, toggleGroups } from '../../utils'
 
-const Group3RankingTable = function({ config, caption, rows, actions }) {
+const Group3RankingTable = function({ config, caption, rows, groups, actions }) {
 
-  const getBackgroundColor = (priority) => {
-    if (priority == 'core') {
-      return '#cfc'
-    } else if (priority < 10) {
-      return '#f7f'
-    } else if (priority < 20) {
-      return '#f8f'
-    } else if (priority < 30) {
-      return '#f9f'
-    } else if (priority < 40) {
-      return '#faf'
-    } else if (priority < 50) {
-      return '#fbf'
-    } else if (priority < 60) {
-      return '#fcf'
-    } else if (priority < 70) {
-      return '#fdf'
-    } else if (priority < 80) {
-      return '#fef'
-    } else {
-      return 'white'
-    }
+  const filteredGroups = filterGroups(config, rows, groups, actions)
+  const allOpen = filteredGroups.every(group => !group.closed)
+  const allToggle = () => toggleGroups(filteredGroups, allOpen)
+
+  const backgroundColor = {
+    'tier1': '#cfc',
+    'tier2': '#f9f',
+    'tier3': '#faf',
+    'tier4': '#fbf',
+    'tier5': '#fcf',
+    'tier6': '#fdf',
+    'tier7': '#fef',
+    'tier8': 'white'
   }
 
   const sortRows = (rows) => {
-    return config.sortRanking ? (
+    return (
       rows.toSorted((a, b) => {
-        if (b.priority == 'core' || a.priority > b.priority) {
+        if (a.priority > b.priority) {
           return 1
-        } else if (a.priority == 'core' || a.priority < b.priority) {
+        } else if (a.priority < b.priority) {
           return -1
         } else {
           return 0
         }
       })
-    ) : (
-      rows
     )
   }
 
@@ -53,31 +42,46 @@ const Group3RankingTable = function({ config, caption, rows, actions }) {
       </caption>
       <thead className="thead-dark">
         <tr>
-          <th style={{width: '20%'}}>Priority</th>
-          <th style={{width: '20%'}}>Climate forcing</th>
-          <th style={{width: '20%'}}>LU model</th>
-          <th style={{width: '40%'}}>Direct human forcing
-            <a href="" className="toggle-group" onClick={(event) => {
-              event.preventDefault()
-              actions.toggleSortRanking()
-            }}>
-              Change sorting
-            </a>
+          <th style={{width: '10%'}}>Priority</th>
+          <th style={{width: '25%'}}>Climate forcing</th>
+          <th style={{width: '25%'}}>LU model</th>
+          <th style={{width: '40%'}}>
+            Direct human forcing
+            <GroupToggleLink className="float-right" closed={!allOpen} toggle={allToggle} all={true} label="tiers" />
           </th>
         </tr>
       </thead>
       <tbody>
         {
-          sortRows(rows).map((row, index) => (
-            <tr key={index}>
-              <td style={{ backgroundColor: getBackgroundColor(row.priority) }}>
-                {row.priority == 'core' ? <strong>Core set</strong> : row.priority}
-              </td>
-              <td>{row.gcm}</td>
-              <td>{row.lu_model}</td>
-              <td><strong>{row.soc_scenario}</strong></td>
-            </tr>
-          ))
+          filteredGroups.map(group => {
+            const header = [
+              <tr key="-1">
+                <td colSpan="5" className="table-secondary">
+                  <GroupToggleLink className="float-right" closed={group.closed} toggle={group.toggle} label="tier"/>
+                  <strong>{group.title}</strong>
+                </td>
+              </tr>
+            ]
+
+            if (group.closed) {
+              return header
+            } else {
+              return header.concat(
+                sortRows(group.rows).map((row, index) => {
+                  return (
+                    <tr key={index}>
+                      <td style={{ backgroundColor: backgroundColor[row.group] }}>
+                        {row.priority}
+                      </td>
+                      <td>{row.gcm}</td>
+                      <td>{row.lu_model}</td>
+                      <td><strong>{row.soc_scenario}</strong></td>
+                    </tr>
+                  )
+                })
+              )
+            }
+          })
         }
       </tbody>
     </table>
