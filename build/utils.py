7d58f83083e559d99a862673f2d16dbb9cf726b0
Jochen Klar <mail@jochenklar.de>
2024-08-23 15:47:58 +0200

    Fix protocol patterns

--- build/utils.py
+++ build/utils.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import re
 import subprocess
 from datetime import datetime
 from pathlib import Path
@@ -82,11 +83,11 @@ def read_patterns(simulation_rounds, sectors):
         patterns[simulation_round['specifier']] = {}
         for sector in sectors:
             if sector.get('simulation_rounds') is None or simulation_round in sector.get('simulation_rounds'):
-                pattern_path = Path('pattern') / simulation_round['specifier'] / 'OutputData' / '{}.json'.format(sector['specifier'])
+                pattern_path = Path('pattern') / simulation_round['specifier'] / 'OutputData' / '{}.yaml'.format(sector['specifier'])
 
                 try:
-                    pattern_list = json.loads(pattern_path.read_text())['file']
-                    patterns[simulation_round['specifier']][sector['specifier']] = '_'.join(pattern_list) + '.nc'
+                    pattern = read_yaml_file(pattern_path)
+                    patterns[simulation_round['specifier']][sector['specifier']] = re.sub(r'\s+', '', pattern['file'])
                 except FileNotFoundError:
                     patterns[simulation_round['specifier']][sector['specifier']] = 'No pattern has been defined for this simulation round and sector, yet.'
 
