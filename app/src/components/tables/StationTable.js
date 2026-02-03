45a7e31427c64928da8f6bfe91694b158ac5b799
Jochen Klar <mail@jochenklar.de>
2026-02-03 14:03:14 +0100

    Fix StationTable component

--- app/src/components/tables/StationTable.js
+++ app/src/components/tables/StationTable.js
@@ -1,6 +1,7 @@
 import React, { Component} from 'react'
 import ReactMarkdown from 'react-markdown'
 import PropTypes from 'prop-types'
+import { orderBy } from 'lodash'
 
 import { filterRows } from '../../utils'
 
@@ -14,29 +15,25 @@ const StationTable = function({ config, caption, rows, actions }) {
         </caption>
         <thead className="thead-dark">
           <tr>
-            <th style={{width: '10%'}}>River Basin</th>
-            <th style={{width: '10%'}}>Station</th>
-            <th style={{width: '20%'}}>Specifier</th>
-            <th style={{width: '15%'}}>Coordinates (Lat, Lon)</th>
-            <th style={{width: '10%'}}>GRDC Station Code</th>
-            <th style={{width: '10%'}}>Data availability (monthly discharge)</th>
-            <th style={{width: '10%'}}>Data availability (daily discharge)</th>
-            <th style={{width: '10%'}}>Area upstream of gauge (km2) according to GRDC or GIS</th>
+            <th style={{width: '20%'}}>River Basin</th>
+            <th style={{width: '20%'}}>Station</th>
+            <th style={{width: '30%'}}>Specifier</th>
+            <th style={{width: '20%'}}>Coordinates (Lat, Lon)</th>
           </tr>
         </thead>
         <tbody>
           {
-            filterRows(config, rows, closed).map((row, index) => {
+            orderBy(filterRows(config, rows), ['river', 'title']).map((row, index) => {
               return (
                 <tr key={index}>
                   <td>{row.river}</td>
                   <td>{row.title}</td>
-                  <td><strong>{row.river_specifier}-{row.specifier}</strong></td>
-                  <td>{row.lat}, {row.lon}</td>
-                  <td>{row.grdc}</td>
-                  <td>{row.availability_monthly}</td>
-                  <td>{row.availability_daily}</td>
-                  <td>{row.area_upstream}</td>
+                  <td><strong>{row.river_specifier}-{row.specifier_file || row.specifier}</strong></td>
+                  <td>
+                    {
+                      row.lat && row.lon && <span>({row.lat}, {row.lon})</span>
+                    }
+                  </td>
                 </tr>
               )
             })
